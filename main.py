7e62f0f30130d61c70812f35735ac54e221e1f5c
tamer bayar <tamerbayr@gmail.com>
2026-03-16 14:56:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -208,8 +208,8 @@ def print_requirements(job: dict):
 def main():
     storage.init_db()
 
-    os.makedirs("docs\cvs", exist_ok=True)
-    os.makedirs("docs\jobs", exist_ok=True)
+    os.makedirs("docs\\cvs", exist_ok=True)
+    os.makedirs("docs\\jobs", exist_ok=True)
     
     print("CV'ler yükleniyor")
     cvs = load_cvs()
